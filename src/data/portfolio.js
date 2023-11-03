3a01896ad8c8e3115bc16d18ba41098795fca321
GSwordGCil <ac983042820@gmail.com>
2023-11-03 03:37:22 -0700

    upload

---
+++ src/data/portfolio.js
@@ -0,0 +1,20 @@
+export default [
+   {
+      title: 'Brain Balance Companion',
+      imgUrl: '/assets/BrainBalance.PNG',
+      stack: ['HTML', 'CSS', 'JavaScript'],
+      link: 'https://kyleochata.github.io/Project-1-SAK/',
+   },
+   {
+      title: 'Hammer Help Desk',
+      imgUrl: '/assets/HelperDesk.PNG',
+      stack: ['SQL', 'Handlebar', 'Node','CSS'],
+      link: 'https://radiant-wave-47724-d875b64f42d0.herokuapp.com/',
+   },
+   {
+      title: 'Super Task Manager',
+      imgUrl: '/assets/placeholder.png', //upcoming
+      stack: ['MongoDB', 'Express', 'React','Node'],
+      link: '/assets/placeholder.png', //upcoming
+   },
+]
